78c67f669742084e58b73405af2af27955ff0004
whatcodehaveyougit <siggywatt@yahoo.com>
2019-12-03 15:59:27 +0000

    delete all added

--- models/property.rb
+++ models/property.rb
@@ -13,6 +13,16 @@ class Property
       @year_built = options['year_built'].to_i
   end
 
+  def Property.all()
+  db = PG.connect({ dbname: 'property_tracker', host: 'localhost' })
+  sql = "SELECT * from propertys_table;"
+  db.prepare("all", sql)
+  property = db.exec_prepared("all")
+  db.close()
+  return property.map { |property_hash| Property.new(property_hash) }
+end
+
+
   def save()
   # In DB is a database connection
   db = PG.connect({ dbname: 'property_tracker', host: 'localhost' })
@@ -30,6 +40,41 @@ class Property
       db.close()
   end
 
-  
+  def update()
+  db = PG.connect({ dbname: 'property_tracker', host: 'localhost' })
+  sql =
+    "UPDATE propertys_table set (
+    address,
+    value,
+    number_of_bedrooms,
+    year_built
+    ) = (
+      $1, $2, $3, $4
+    ) WHERE id = $5;
+    "
+      values = [@address, @value, @number_of_bedrooms, @year_built, @id]
+      db.prepare("update", sql)
+      db.exec_prepared("update", values)
+      db.close()
+  end
+
+
+  def delete()
+      db = PG.connect({ dbname: 'property_tracker', host: 'localhost'})
+      sql = "DELETE FROM propertys_table WHERE id = $1"
+      values = [@id]
+      db.prepare("delete_one", sql)
+      db.exec_prepared("delete_one", values)
+      db.close
+    end
+
+    def Property.delete_all()
+        db = PG.connect({ dbname: 'property_tracker', host: 'localhost' })
+        sql = "DELETE FROM propertys_table"
+        db.prepare("delete_all", sql)
+        db.exec_prepared("delete_all")
+        db.close()
+    end
+
 
 end
